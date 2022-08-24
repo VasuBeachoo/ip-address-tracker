eab56f9b96565d96d20bb905edb4538f838338c6
Vasu Beachoo <vasubeachoo@gmail.com>
2022-08-23 20:58:36 -0400

    Style info display block

--- src/components/DisplayDetail.js
+++ src/components/DisplayDetail.js
@@ -1,14 +1,38 @@
 import styled from "styled-components";
 
-export const DetailValue = styled.p``;
+export const DetailValue = styled.p`
+  color: var(--Very-Dark-Gray);
+  font-size: 1.35rem;
+  font-weight: 500;
+  line-height: 1.85ch;
+  max-width: 20ch;
+  margin: 0;
 
-export const DetailLabel = styled.h3``;
+  @media (max-width: 1000px) {
+    max-width: 30ch;
+  }
+`;
+
+export const DetailLabel = styled.h3`
+  color: var(--Dark-Gray);
+  font-size: 0.75rem;
+  font-weight: 700;
+  letter-spacing: 0.15ch;
+  margin: 0;
+`;
 
 export const DetailBox = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
   align-items: flex-start;
+  gap: 0.5rem;
+  width: 100%;
+
+  @media (max-width: 1000px) {
+    align-items: center;
+    text-align: center;
+  }
 `;
 
 const DisplayDetail = ({ className, label, value }) => {
--- src/components/InfoDisplay.js
+++ src/components/InfoDisplay.js
@@ -1,25 +1,47 @@
 import styled from "styled-components";
 import DisplayDetail from "./DisplayDetail";
 
-export const Divider = styled.hr``;
+export const Divider = styled.hr`
+  height: 4rem;
+  opacity: 0.3;
+  margin: 0;
+
+  @media (max-width: 1000px) {
+    display: none;
+  }
+`;
 
 export const DisplayBox = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-between;
-  align-items: center;
+  justify-content: flex-start;
+  align-items: flex-start;
+  gap: 1.5rem;
+  width: clamp(10rem, 80vw, 70rem);
+  background: var(--White);
+  border-radius: 0.75rem;
+  box-shadow: rgba(0, 0, 0, 0.1) 0px 10px 15px -3px,
+    rgba(0, 0, 0, 0.05) 0px 4px 6px -2px;
+  padding: 2rem 2.5rem;
+
+  @media (max-width: 1000px) {
+    flex-direction: column;
+    align-items: center;
+    gap: 2rem;
+    padding: 2.65rem 1rem;
+  }
 `;
 
 const InfoDisplay = ({ className }) => {
   return (
     <DisplayBox className={className}>
-      <DisplayDetail label="IP ADDRESS" value="value" />
+      <DisplayDetail label="IP ADDRESS" value="192.212.174.101" />
       <Divider />
-      <DisplayDetail label="LOCATION" value="value" />
+      <DisplayDetail label="LOCATION" value="Brooklyn, NY 10001" />
       <Divider />
-      <DisplayDetail label="TIMEZONE" value="value" />
+      <DisplayDetail label="TIMEZONE" value="UTC -05:00" />
       <Divider />
-      <DisplayDetail label="ISP" value="value" />
+      <DisplayDetail label="ISP" value="SpaceX Starlink" />
     </DisplayBox>
   );
 };
