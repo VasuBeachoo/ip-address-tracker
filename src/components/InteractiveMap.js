98def1ae5a3f700cfc5637983ebebfb6d996480d
Vasu Beachoo <vasubeachoo@Vasus-MacBook-Air.local>
2022-08-27 16:48:07 -0400

    Make initial map position current location

--- src/components/InteractiveMap.js
+++ src/components/InteractiveMap.js
@@ -1,21 +1,50 @@
+import { useState, useEffect } from "react";
 import Map, {
   FullscreenControl,
   GeolocateControl,
   NavigationControl,
+  Marker,
 } from "react-map-gl";
+import iconMarker from "../assets/icon-location.svg";
 
 const InteractiveMap = () => {
+  const [viewState, setViewState] = useState({
+    longitude: -100,
+    latitude: 40,
+    zoom: 13,
+  });
+  const [markerLoc, setMarkerLoc] = useState({ longitude: -100, latitude: 40 });
+
+  useEffect(() => {
+    navigator.geolocation &&
+      navigator.geolocation.getCurrentPosition((position) => {
+        setViewState({
+          ...viewState,
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+        });
+        setMarkerLoc({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      });
+  }, []);
+
   return (
     <Map
-      initialViewState={{
-        longitude: -100,
-        latitude: 40,
-        zoom: 3.5,
-      }}
+      {...viewState}
+      onMove={(e) => setViewState(e.viewState)}
       style={{ position: "relative", width: "100%", height: "100vh" }}
       mapStyle="mapbox://styles/mapbox/satellite-streets-v11"
       mapboxAccessToken="pk.eyJ1IjoiZ2FtZW1hc3RlcjUwMDAiLCJhIjoiY2w3ODF4Yzh3MDQ0dDNvcXJ1bmQycWR2NyJ9.usvwfx-Sevyl-S8ut45bVQ"
     >
+      <Marker
+        longitude={markerLoc.longitude}
+        latitude={markerLoc.latitude}
+        anchor="bottom"
+      >
+        <img src={iconMarker} alt="location-marker-icon" />
+      </Marker>
       <FullscreenControl />
       <GeolocateControl />
       <NavigationControl />
